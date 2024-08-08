55e8c3c9ef15274aed48271c659d016deedb2c36
Kimmo Kinnunen <kkinnunen@apple.com>
2024-08-08 14:01:38 +0300

    Update ANGLE to 2024-08-08 (d335f134eaae1615612bd82799d5f3e0f38caf27) Need the bug URL (OOPS!). Include a Radar link (OOPS!).
    Reviewed by NOBODY (OOPS!)
    
    Contains upstream commits:
    git log --oneline 26aa4df52789a9079ce6cc7d18ff055c6043ccc9..d335f134eaae1615612bd82799d5f3e0f38caf27 --pretty=%h %s
    d335f134ea Roll Chromium from 0023a97ccc68 to 26ff6a053683 (629 revisions)
    81c01d2f81 Add an option to output baked angle git hash to angle_commit.h
    1010a275d4 CL/Vulkan: Set proper build status prior to API return
    bc0c6c5bb6 VVL: sync_queue_submit -> syncval_submit_time_validation
    27c0f10a95 WebGPU: Generate texture formats when initializing surfaces.
    057bc0f0f3 WebGPU: Set multiple parameters in RenderPipelineDesc.
    8a8b85bbde Metal: Adjust supports32BitFloatFiltering compile guards
    7549427ff6 Roll vulkan-deps from 3d8509997599 to b4b1cdd9bdc8 (8 revisions)
    0592b89735 Metal: fix frame capture bundle file permissions
    bb747f55b3 Roll Chromium from 53e46965d0e3 to 0023a97ccc68 (589 revisions)
    70f2e2033b Metal: Fix memoryless usage on ios on tv
    7a81e79d44 Vulkan: Fix mPipelineCache not populated from blob cache
    721becf2d4 Vulkan: Update Renderer::syncPipelineCacheVk() method
    3e4fa12868 Store ImageHelper's queue by mip levels.
    67d8d8c88a WebGL: Reject shaders with infinite loops
    d003d9e29a Manual roll vulkan-deps from 068847956e41 to 3d8509997599 (49 revisions)
    791970d415 Fix Dawn Android standalone build.
    c5d4c16f4f WebGPU: Initialize all GLES 2 and 3 caps
    91f57b06fd Roll SwiftShader from f72761e86766 to 65157d32945d (1 revision)
    81eaa2dd95 Roll Chromium from af4b4ec4b6b5 to 53e46965d0e3 (504 revisions)
    2ae68a2086 Add MRT test for foveated rendering feature
    9b06e0038c Vulkan: Avoid unnecessary observer changes for Buffers
    b7b46eed07 Updates to readme
    a89ca6c810 Metal: Fully implement unresolve step for MSRTT.
    1bfe5c5741 Metal: partially implement EXT_multisampled_render_to_texture.
    9410b29e6b Roll chromium_revision df89992288..af4b4ec4b6 (1336472:1337199)

--- Source/ThirdParty/ANGLE/src/libANGLE/renderer/metal/mtl_state_cache.h
+++ Source/ThirdParty/ANGLE/src/libANGLE/renderer/metal/mtl_state_cache.h
@@ -321,6 +321,11 @@ struct RenderPassAttachmentDesc
 
     ANGLE_INLINE bool hasResolveTexture() const { return resolveTexture.get(); }
 
+    const TextureRef &getImplicitMSTextureIfAvailOrTexture() const
+    {
+        return texture ? texture : resolveTexture;
+    }
+
     // When rendering with implicit multisample, |texture| is the texture that
     // will be rendered into and discarded at the end of a render pass. Its
     // result will be automatically resolved into |resolveTexture|.
